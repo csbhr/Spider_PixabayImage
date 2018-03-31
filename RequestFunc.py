3515ad39b40ea5b9deaa7dec87fcbe2108d78a7f
csbhr <931937713@qq.com>
2018-03-31 22:26:14 +0800

    version-3 for search image with pageNumber

--- RequestFunc.py
+++ RequestFunc.py
@@ -3,22 +3,10 @@ import requests
 import html
 
 # 从搜索word得到的搜索界面获取图片的详情页连接
-def get_SearchPageWord_Image_DetailUrl(word):
+def get_SearchPageWord_Image_DetailUrl(word,pageNumber):
     # 获取html
-    url = "https://pixabay.com/zh/photos/?q=" + word + "&hp=&image_type=all&order=&cat=&min_width=&min_height="
+    url = "https://pixabay.com/zh/photos/?min_height=&image_type=all&cat=&q=" + word + "&min_width=&order=&pagi="+str(pageNumber)
     res = requests.get(url)
-    body = html.unescape(res.text)
-    # 匹配图片详情页连接的正则表达式
-    re_Image_DetailUrl = re.compile("<div class=\"item\" data-w=\".*?\" data-h=\".*?\"><a href=\"(.*?)\">", re.S)
-    # 匹配得到图片详情页连接的列表
-    list_Image_DetailUrl = re_Image_DetailUrl.findall(body)
-    # 返回列表
-    return list_Image_DetailUrl
-
-# 从搜索连接得到的搜索界面获取图片的详情页连接
-def get_SearchPageUrl_Image_DetailUrl(searchPageUrl):
-    # 获取html
-    res = requests.get("https://pixabay.com/"+searchPageUrl)
     body = html.unescape(res.text)
     # 匹配图片详情页连接的正则表达式
     re_Image_DetailUrl = re.compile("<div class=\"item\" data-w=\".*?\" data-h=\".*?\"><a href=\"(.*?)\">", re.S)
@@ -38,15 +26,3 @@ def get_DetailPageUrl_Image_Url(detailPageUrl):
     list_Image_Url = re_Image_Url.findall(body)
     # 由于一个详情页只有一个图片，则返回列表第一个
     return list_Image_Url[0]
-
-# 从图片的详情页提取拓展的搜索页连接
-def get_DetailPageUrl_SeachPage_Url(detailPageUrl):
-    # 获取html
-    res = requests.get("https://pixabay.com/"+detailPageUrl)
-    body = html.unescape(res.text)
-    # 匹配搜索页连接的正则表达式
-    re_SearchPage_Url = re.compile("<a href=\"(.*?)\">.*?</a> ", re.S)
-    # 匹配得到搜索页连接
-    list_SearchPage_Url = re_SearchPage_Url.findall(body)
-    # 返回列表
-    return list_SearchPage_Url
--- main.py
+++ main.py
@@ -1,35 +1,28 @@
 import RequestFunc
 import FileFunc
 import time
-import sys
 
-def get_Image_Urls(list_DetailUrl, list_Urls):
-    if(len(list_Urls)>=2000):
-        print("Distincting...")
-        set_Urls = set(list_Urls)
-        print("Get " + str(len(set_Urls)) + " Images.")
-        print("Writing to urls.txt...")
-        FileFunc.writeTxt("urls.txt", set_Urls)
-        print("Writing over.")
-        sys.exit(0)
-    for durl in list_DetailUrl:
-        imageUrl=RequestFunc.get_DetailPageUrl_Image_Url(durl)
-        list_Urls.append(imageUrl)
-        print(time.strftime("%Y-%m-%d %H:%M:%S",time.localtime(time.time()))+" >> Test Number"+str(len(list_Urls))+" : "+imageUrl)
-    for durl in list_DetailUrl:
-        extend_List_SearchUrl=RequestFunc.get_DetailPageUrl_SeachPage_Url(durl)
-        extend_List_SearchUrl.sort()
-        for elsurl in extend_List_SearchUrl:
-            extend_List_DetailUrl=RequestFunc.get_SearchPageUrl_Image_DetailUrl(elsurl)
-            get_Image_Urls(extend_List_DetailUrl,list_Urls)
+# 根据关键字获取图片图片连接
+def get_Image_Urls(word):
+    # 使用集合set存储连接，用于去重
+    set_image_urls = set([])
+    # 利用关键字word搜索图片，取1到10页图片
+    for pageNumber in range(1, 11):
+        image_detail_urls = RequestFunc.get_SearchPageWord_Image_DetailUrl(word, pageNumber)
+        for durl in image_detail_urls:
+            image_url = RequestFunc.get_DetailPageUrl_Image_Url(durl)
+            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + " >> Test : " + image_url)
+            set_image_urls.add(image_url)
+    return set_image_urls
 
-
-list_DetailUrl=RequestFunc.get_SearchPageWord_Image_DetailUrl("风景")
-list_Urls=[]
-get_Image_Urls(list_DetailUrl,list_Urls)
-print("Distincting...")
-set_Urls=set(list_Urls)
-print("Get "+str(len(set_Urls))+" Images.")
-print("Writing to urls.txt...")
-FileFunc.writeTxt("urls.txt", set_Urls)
-print("Writing over.")
+# 用户循环输入关键字word，当用户输入exit使退出程序
+msg=input("Please input keyword:")
+while msg!="exit":
+    set_image_urls=get_Image_Urls(msg)
+    print("KeyWord:"+msg+" Get "+str(len(set_image_urls))+" images.")
+    # 将获取的连接存储到以关键字命名的文件中
+    fileName=msg+".txt"
+    print("Write image urls into "+fileName+"......")
+    FileFunc.writeTxt(fileName, set_image_urls)
+    print("Write over......\n")
+    msg=input("Please input keyword:")
