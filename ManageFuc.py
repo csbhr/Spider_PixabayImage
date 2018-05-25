59b807fad21c09ca048e9ac05b9027d77270b82b
csbhr <931937713@qq.com>
2018-05-25 17:06:20 +0800

    clear

--- ManageFuc.py
+++ ManageFuc.py
@@ -5,26 +5,25 @@ import requests
 import os
 
 # 根据关键字获取图片图片连接
-def get_Image_Urls(word):
+def get_Image_Urls(word,pageNum):
     # 使用集合set存储连接，用于去重
     set_image_urls = set([])
     # 利用关键字word搜索图片，取1到5页图片
-    for pageNumber in range(1, 2):
-        image_detail_urls = RequestFunc.get_SearchPageWord_Image_DetailUrl(word, pageNumber)
-        for durl in image_detail_urls:
-            image_url = RequestFunc.get_DetailPageUrl_Image_Url(durl)
-            print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + " >> Test : " + image_url)
-            set_image_urls.add(image_url)
+    image_detail_urls = RequestFunc.get_SearchPageWord_Image_DetailUrl(word, pageNum)
+    for durl in image_detail_urls:
+        image_url = RequestFunc.get_DetailPageUrl_Image_Url(durl)
+        print(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime(time.time())) + " >> Test : " + image_url)
+        set_image_urls.add(image_url)
     return set_image_urls
 
 # 负责业务的组织管理
-def manage(word):
+def manage(word,pageNum):
     # 获取图片连接
-    set_image_urls = get_Image_Urls(word)
+    set_image_urls = get_Image_Urls(word,pageNum)
     print("KeyWord:" + word + " Get " + str(len(set_image_urls)) + " images.")
 
     # 将获取的连接存储到以关键字命名的文件中
-    fileName = word + ".txt"
+    fileName = word +str(pageNum)+ ".txt"
     print("Write image urls into " + fileName + "......")
     FileFunc.writeTxt(fileName, set_image_urls)
     print("Write over......\n")
@@ -32,12 +31,13 @@ def manage(word):
     # 将文件中存储的连接下载成文件
     set_image_urls = FileFunc.readTxt(fileName)
     # 以关键字创建目录
-    if not os.path.exists(word):
-        os.makedirs(word)
+    dirName=word+str(pageNum)
+    if not os.path.exists(dirName):
+        os.makedirs(dirName)
     # 下载图片
     for url in set_image_urls:
         res = requests.get(url)
-        imageName = word+"\\" + (url[30:]).replace("/", "&")
+        imageName = dirName+"\\" + (url[30:]).replace("/", "&")
         print("Download " + imageName + "......")
         with open(imageName, "wb") as theFile:
             theFile.write(res.content)
--- main.py
+++ main.py
@@ -1,17 +1,17 @@
 ﻿import ManageFuc
 import time
 
-
+'''
 # 用户循环输入关键字word，当用户输入exit使退出程序
 word=input("Please input keyword:")
 while word!="exit":
-    ManageFuc.manage(word)
+    ManageFuc.manage(word,1)
     word=input("Please input keyword:")
 
+'''
 
-'''
-list_word=["山川","流水","树木","花草","田园","明星","模特","歌手","农民","工人","狗","猫","马","鸟","鱼","衣服","玩具","家具","房子","电器"]
-for word in list_word[13:14]:
-    ManageFuc.manage(word)
-    time.sleep(5)
-'''
+list_word=["雨","龙卷风","云","地震","龙","神仙","西游","外星人","火影","海贼王"]
+for word in list_word[3:]:
+    for pageNum in range(1,9):
+        ManageFuc.manage(word,pageNum)
+        time.sleep(60)
